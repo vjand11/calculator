ce1a1c374f8adc27fb229f676bd1f7dc8a45d361
Virginia Anderson <virginia.j.schmidt@gmail.com>
2022-06-06 15:47:33 -0400

    added logic to select operator

--- src/virginia/com/Main.java
+++ src/virginia/com/Main.java
@@ -1,6 +1,8 @@
 package virginia.com;
 
 import javax.sound.midi.Soundbank;
+import java.io.IOException;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
@@ -11,27 +13,34 @@ public class Main {
         String firstNum = scanner.next();
         System.out.println("Enter your second number: ");
         String secondNum = scanner.next();
+        System.out.println("Enter the operation: ");
+        String operation = scanner.next();
+        String lowerCaseOp = operation.toLowerCase();
         System.out.println();
-
-        System.out.println("...now adding...");
-        Add add = new Add();
-        add.calculate(Double.parseDouble(firstNum), Double.parseDouble(secondNum));
-        System.out.println();
-
-        System.out.println("...now subtracting...");
-        Subtract subtract = new Subtract();
-        subtract.calculate(Double.parseDouble(firstNum), Double.parseDouble(secondNum));
-        System.out.println();
-
-        System.out.println("...now multiplying...");
-        Multiply multiply = new Multiply();
-        multiply.calculate(Double.parseDouble(firstNum), Double.parseDouble(secondNum));
-        System.out.println();
-
-        System.out.println("...now dividing...");
-        Divide divide = new Divide();
-        divide.calculate(Double.parseDouble(firstNum), Double.parseDouble(secondNum));
-
+        
+        try {
+            if (lowerCaseOp.equals("add")) {
+                System.out.println("...now adding...");
+                Add addition = new Add();
+                addition.calculate(Double.parseDouble(firstNum), Double.parseDouble(secondNum));
+            } else if (lowerCaseOp.equals("subtract")) {
+                System.out.println("...now subtracting...");
+                Subtract subtract = new Subtract();
+                subtract.calculate(Double.parseDouble(firstNum), Double.parseDouble(secondNum));
+            } else if (lowerCaseOp.equals("multiply")) {
+                System.out.println("...now multiplying...");
+                Multiply multiply = new Multiply();
+                multiply.calculate(Double.parseDouble(firstNum), Double.parseDouble(secondNum));
+            } else if (lowerCaseOp.equals("divide")) {
+                System.out.println("...now dividing...");
+                Divide divide = new Divide();
+                divide.calculate(Double.parseDouble(firstNum), Double.parseDouble(secondNum));
+            } else {
+                System.out.println("oh no");
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println("Invalid input: " + e.getMessage());
+        }
     }
 
 }
